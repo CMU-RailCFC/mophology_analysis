9cfb73a8c0df75f281f8232622c3fdc88d20dd0e
Unknown-user-C <42628079+jangchiang@users.noreply.github.com>
2023-10-02 23:39:09 +1100

    update

--- test.py
+++ test.py
@@ -1,24 +1,10 @@
 import os
-from ballast_analyzer.analyzer import analyze_ballast
+from analyzer import analyze_ballast, analyze_multiple_files
 
-# Use the analyze_ballast function here with the path to your 3D model file
-# to run all obj file in the folder please use the first option, the second option for run single obj file
-#option 1
-'''
-def analyze_all_files_in_folder(folder_path, file_extension=".obj"):
-    # List all files in the folder
-    files = os.listdir(folder_path)
-
-    # Filter files by the specified file extension
-    files = [f for f in files if f.endswith(file_extension)]
-
-    # Analyze all files with the specified file extension
-    for file in files:
-        file_path = os.path.join(folder_path, file)
-        result = analyze_ballast(file_path)
-
-        # Print the results in the desired format
-        print("subjectname:", result["Filename"])
+def analyze_single_file(file_path):
+    result = analyze_ballast(file_path)
+    if result:
+        print("Filename:", result["Filename"])
         print("Intermediate:", result["Intermediate"])
         print("Shortest:", result["Shortest"])
         print("Longest:", result["Longest"])
@@ -28,33 +14,27 @@ def analyze_all_files_in_folder(folder_path, file_extension=".obj"):
         print("Sphericity:", result["Sphericity"])
         print("Roundness:", result["Roundness"])
         print("Roughness:", result["Roughness"])
-        print("Sphere center:", (result["Center X"], result["Center Y"], result["Center Z"]))
-        print("Sphere radius:", result["Radius"])
-        print("Sphere fit:", ((result["Center X"], result["Center Y"], result["Center Z"]), result["Radius"]))
-        print("Angularity Index:", result["Angularity Index"])
+        print("Sphere Center:", (result["Center X"], result["Center Y"], result["Center Z"]))
+        print("Sphere Radius:", result["Radius"])
         print("Aspect Ratio:", result["Aspect Ratio"])
-
-
+        print("Angularity Index:", result["Angularity Index"])
         print("\n---\n")
 
-folder_path = "path/to/your/folder"
-analyze_all_files_in_folder(folder_path)
-'''
-#option 2
-result = analyze_ballast('U15_1/U15_1.stl')
-# Print the results in the desired format
-print("subjectname:", result["Filename"])
-print("Intermediate:", result["Intermediate"])
-print("Shortest:", result["Shortest"])
-print("Longest:", result["Longest"])
-print("Elongation:", result["Elongation"])
-print("Flatness:", result["Flatness"])
-print("Convexity:", result["Convexity"])
-print("Sphericity:", result["Sphericity"])
-print("Roundness:", result["Roundness"])
-print("Roughness:", result["Roughness"])
-print("Sphere center:", (result["Center X"], result["Center Y"], result["Center Z"]))
-print("Sphere radius:", result["Radius"])
-print("Sphere fit:", ((result["Center X"], result["Center Y"], result["Center Z"]), result["Radius"]))
-print("Angularity Index:", result["angularity index"])
-print("Aspect Ratio:", result["Aspect Ratio"])
+def analyze_all_files_in_folder(folder_path, num_cores=None, file_extension=".obj"):
+    files = [f for f in os.listdir(folder_path) if f.endswith(file_extension)]
+    files = [os.path.join(folder_path, f) for f in files]
+
+    results = analyze_multiple_files(files, num_cores)
+    for result in results:
+        if result:
+            print("Filename:", result["Filename"])
+            # ... (same as above, printing other properties)
+            print("\n---\n")
+
+# Analyzing a single file
+single_file_path = "/path/to/your/single/model/file.obj"
+analyze_single_file(single_file_path)
+
+# Analyzing all files in a folder
+# folder_path = "/path/to/your/models"
+# analyze_all_files_in_folder(folder_path, num_cores=4)
