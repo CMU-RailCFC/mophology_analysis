8d5ccbe38af5037a098e9ac560e9036561285111
Unknown-user-C <42628079+jangchiang@users.noreply.github.com>
2023-10-07 05:21:09 +1100

    update the class oackage

--- ballast_analyzer/analyzer.py
+++ ballast_analyzer/analyzer.py
@@ -29,80 +29,101 @@ import csv
 from scipy.optimize import least_squares
 from concurrent.futures import ProcessPoolExecutor
 
-def sphere_fit_function(parameters, points):
-    center_x, center_y, center_z, radius = parameters
-    center = np.array([center_x, center_y, center_z])
-    residual = np.linalg.norm(points - center, axis=1) - radius
-    return residual
+class BallastAnalyzer:
+    def __init__(self):
+        pass
 
-def fit_sphere(points):
-    center = np.mean(points, axis=0)
-    radius = np.mean(np.linalg.norm(points - center, axis=1))
-    initial_guess = np.array([center[0], center[1], center[2], radius])
-    result = least_squares(sphere_fit_function, initial_guess, args=(points,))
-    return result.x[0:3], result.x[3]
+    def _sphere_fit_function(self, parameters, points):
+        center_x, center_y, center_z, radius = parameters
+        center = np.array([center_x, center_y, center_z])
+        residual = np.linalg.norm(points - center, axis=1) - radius
+        return residual
 
-def analyze_ballast(filename):
-    model = trimesh.load(filename)
-    surface_vertices = model.vertices
-    center, radius = fit_sphere(surface_vertices)
-    
-    dimensions = model.bounding_box.primitive.extents
-    intermediate = (dimensions[0] + dimensions[1] + dimensions[2]) / 3
-    shortest = min(dimensions)
-    longest = max(dimensions)
-    
-    elongation = intermediate / longest
-    flatness = shortest / intermediate
-    convexity = model.convex_hull.volume / model.volume
-    sphericity = (3 * (4 * np.pi * model.volume) / (model.area ** 2)) ** (1/3)
-    
-    dists = np.linalg.norm(surface_vertices - center, axis=1)
-    mean_dist = np.mean(dists - radius)
-    roughness = mean_dist
-    roundness = (mean_dist / radius)
-    
-    aspect_ratio = longest / shortest
-    actual_volume = model.volume
-    
-    bounding_box = model.bounding_box_oriented
-    bounding_box_dimensions = bounding_box.extents
-    bounding_box_volume = np.prod(bounding_box_dimensions)
-    
-    angularity_index = actual_volume / bounding_box_volume
-    
-    data = {
-        'Filename': filename,
-        'Intermediate': intermediate,
-        'Shortest': shortest,
-        'Longest': longest,
-        'Elongation': elongation,
-        'Flatness': flatness,
-        'Convexity': convexity,
-        'Sphericity': sphericity,
-        'Roundness': roundness,
-        'Roughness': roughness,
-        'Center X': center[0],
-        'Center Y': center[1],
-        'Center Z': center[2],
-        'Radius': radius,
-        'Aspect Ratio': aspect_ratio,
-        'Angularity Index': angularity_index,
-    }
-    
-    csv_file_path = 'data.csv'
-    if not os.path.isfile(csv_file_path):
-        with open(csv_file_path, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=data.keys())
-            writer.writeheader()
+    def _fit_sphere(self, points):
+        center = np.mean(points, axis=0)
+        radius = np.mean(np.linalg.norm(points - center, axis=1))
+        initial_guess = np.array([center[0], center[1], center[2], radius])
+        result = least_squares(self._sphere_fit_function, initial_guess, args=(points,))
+        return result.x[0:3], result.x[3]
 
-    with open(csv_file_path, 'a', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=data.keys())
-        writer.writerow(data)
-    
-    return data
+    def _save_point_cloud(self, vertices, filename):
+        if not os.path.exists('point_clouds'):
+            os.makedirs('point_clouds')
+        np.savetxt(f"point_clouds/{filename}_point_cloud.csv", vertices, delimiter=",")
 
-def analyze_multiple_files(filenames, num_cores=None):
-    with ProcessPoolExecutor(max_workers=num_cores) as executor:
-        results = list(executor.map(analyze_ballast, filenames))
-    return results
+    def _save_mesh(self, mesh, filename):
+        if not os.path.exists('meshes'):
+            os.makedirs('meshes')
+        mesh.export(f"meshes/{filename}.stl")
+
+    def analyze_ballast(self, filename):
+        try:
+            model = trimesh.load(filename)
+            surface_vertices = model.vertices
+            center, radius = self._fit_sphere(surface_vertices)
+            
+            dimensions = model.bounding_box.primitive.extents
+            intermediate = np.mean(dimensions)
+            shortest = min(dimensions)
+            longest = max(dimensions)
+
+            elongation = intermediate / longest
+            flatness = shortest / intermediate
+            convexity = model.convex_hull.volume / model.volume
+            sphericity = (np.pi * (3 * model.volume) / (model.area ** 2)) ** (1/3)
+            
+            dists = np.linalg.norm(surface_vertices - center, axis=1)
+            mean_dist = np.mean(dists - radius)
+            roughness = mean_dist
+            roundness = mean_dist / radius
+            
+            aspect_ratio = longest / shortest
+            actual_volume = model.volume
+
+            bounding_box = model.bounding_box_oriented
+            bounding_box_dimensions = bounding_box.extents
+            bounding_box_volume = np.prod(bounding_box_dimensions)
+
+            angularity_index = actual_volume / bounding_box_volume
+            
+            data = {
+                'Filename': filename,
+                'Intermediate': intermediate,
+                'Shortest': shortest,
+                'Longest': longest,
+                'Elongation': elongation,
+                'Flatness': flatness,
+                'Convexity': convexity,
+                'Sphericity': sphericity,
+                'Roundness': roundness,
+                'Roughness': roughness,
+                'Center X': center[0],
+                'Center Y': center[1],
+                'Center Z': center[2],
+                'Radius': radius,
+                'Aspect Ratio': aspect_ratio,
+                'Angularity Index': angularity_index,
+            }
+
+            csv_file_path = 'data.csv'
+            if not os.path.isfile(csv_file_path):
+                with open(csv_file_path, 'w', newline='') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+                    writer.writeheader()
+
+            with open(csv_file_path, 'a', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+                writer.writerow(data)
+            
+            self._save_point_cloud(surface_vertices, os.path.basename(filename).split('.')[0])
+            self._save_mesh(model, os.path.basename(filename).split('.')[0])
+
+            return data
+        except Exception as e:
+            print(f"An error occurred while processing {filename}: {e}")
+            return None
+
+    def analyze_multiple_files(self, filenames, num_cores=None):
+        with ProcessPoolExecutor(max_workers=num_cores) as executor:
+            results = list(executor.map(self.analyze_ballast, filenames))
+        return results
--- test.py
+++ test.py
@@ -1,61 +1,41 @@
 import os
-from analyzer import analyze_ballast, analyze_multiple_files
 import logging
+from analyzer import BallastAnalyzer
 
 # Configure logging to print detailed information
 logging.basicConfig(level=logging.DEBUG)
 
-def analyze_single_file(file_path):
-    result = analyze_ballast(file_path)
+def analyze_single_file(analyzer, file_path):
+    result = analyzer.analyze_ballast(file_path)
     if result:
-        print("Filename:", result["Filename"])
-        print("Intermediate:", result["Intermediate"])
-        print("Shortest:", result["Shortest"])
-        print("Longest:", result["Longest"])
-        print("Elongation:", result["Elongation"])
-        print("Flatness:", result["Flatness"])
-        print("Convexity:", result["Convexity"])
-        print("Sphericity:", result["Sphericity"])
-        print("Roundness:", result["Roundness"])
-        print("Roughness:", result["Roughness"])
-        print("Sphere Center:", (result["Center X"], result["Center Y"], result["Center Z"]))
-        print("Sphere Radius:", result["Radius"])
-        print("Aspect Ratio:", result["Aspect Ratio"])
-        print("Angularity Index:", result["Angularity Index"])
+        for key, value in result.items():
+            print(f"{key}: {value}")
         print("\n---\n")
 
-def analyze_all_files_in_folder(folder_path, num_cores=None, file_extension=".stl"):
+def analyze_all_files_in_folder(analyzer, folder_path, num_cores=None, file_extension=".stl"):
     files = [f for f in os.listdir(folder_path) if f.endswith(file_extension)]
     files = [os.path.join(folder_path, f) for f in files]
     logging.info(f"Analyzing {len(files)} files in folder: {folder_path}")
 
     for file in files:
         try:
-            results = analyze_multiple_files([file], num_cores)  # Analyzing files one by one
+            results = analyzer.analyze_multiple_files([file], num_cores)
             for result in results:
                 if result:
-                	print("Filename:", result["Filename"])
-                	print("Intermediate:", result["Intermediate"])
-                	print("Shortest:", result["Shortest"])
-                	print("Longest:", result["Longest"])
-                	print("Elongation:", result["Elongation"])
-                	print("Flatness:", result["Flatness"])
-                	print("Convexity:", result["Convexity"])
-                	print("Sphericity:", result["Sphericity"])
-                	print("Roundness:", result["Roundness"])
-                	print("Roughness:", result["Roughness"])
-                	print("Sphere Center:", (result["Center X"], result["Center Y"], result["Center Z"]))
-                	print("Sphere Radius:", result["Radius"])
-                	print("Aspect Ratio:", result["Aspect Ratio"])
-                	print("Angularity Index:", result["Angularity Index"])
-                	print("\n---\n")
+                    for key, value in result.items():
+                        print(f"{key}: {value}")
+                    print("\n---\n")
         except Exception as e:
             logging.error(f"An error occurred while analyzing the file {file}: {e}")
-            continue  # Skip the file and continue with the next one
+            continue
+
+# Create an analyzer object
+analyzer = BallastAnalyzer()
+
 # Analyzing a single file
-#single_file_path = "/path/to/your/single/model/file.obj"
-#analyze_single_file(single_file_path)
+# single_file_path = "/path/to/your/single/model/file.stl"
+# analyze_single_file(analyzer, single_file_path)
 
 # Analyzing all files in a folder
 folder_path = "/home/railcmuvpn/Documents/Morphology_CFC/model"
-analyze_all_files_in_folder(folder_path, num_cores=4)
+analyze_all_files_in_folder(analyzer, folder_path, num_cores=4)
